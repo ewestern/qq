9acafe88d326e4bacbd1c4bf390973363fa0ebe7
Peter France <peter@narrative.io>
2018-06-26 07:40:00 -0400

    placeholder for error

--- app/Main.hs
+++ app/Main.hs
@@ -35,17 +35,21 @@ run p s = case parse (p <* eof) "Command-line SQL"  s of
 
 type Delimiter = Char
 
-printer :: Maybe (V.Vector Prim) ->  IO ()
+printer :: Maybe Row ->  IO ()
 printer Nothing = return ()
-printer (Just row) = BC.putStr $ encode $ encodeRecord row
+printer (Just row) = BC.putStr $ encode $ encodeRecord $ V.map swallowError row
+-- print (Just (Left err)) = return ()
 
+swallowError :: ErrR Prim -> Prim
+swallowError (Right p) = p
+swallowError (Left e) = StringPrim "ERR"
 
-printSink  :: IO (OutputStream (V.Vector Prim))
+printSink  :: IO (OutputStream Row)
 printSink =  makeOutputStream printer
 
-printHeader :: OutputStream (V.Vector Prim) -> Maybe Header -> IO ()
+printHeader :: OutputStream Row  -> Maybe Header -> IO ()
 printHeader os = \case 
-  Just h -> write (Just $ V.map StringPrim h) os
+  Just h -> write (Just $ V.map (Right . StringPrim) h) os
   Nothing -> return ()
 
 query :: Args -> IO ()
@@ -53,11 +57,11 @@ query a@(Args command options) = do
   let ctx = Context M.empty M.empty Nothing $ SelectHeaderMap M.empty
       queryExpr = run parseQueryExpr command
   init <- getInitial options queryExpr
-  eitherTuple <- runExceptT $ flip runStateT ctx ( plan (parallelism options) init queryExpr >>= execute  >>= liftIO . (SC.map (fmap throwEither)))
+-- TODO: figure out what to do with Row Errors!!
+  eitherTuple <- runExceptT $ flip runStateT ctx ( plan (parallelism options) init queryExpr >>= execute) --   >>= liftIO . (SC.map (fmap throwEither)))
   sink <- printSink
   case eitherTuple of
     Right (input, context) -> do 
-        print $ _selectHeader context
         printHeader sink (_selectHeader context)
         connect input sink
     Left s -> error "ASD"
